d718cea2669b84cdda6d5b19f3a23d7d84c911fe
Nastya <Anastasia.Fedosova@urfu.me>
2025-07-02 07:11:55 +0500

    Update param comment

--- homework_autograd.py
+++ homework_autograd.py
@@ -4,96 +4,96 @@ import torch
 
 # Задание 2.1
 
-# # Создаем тензоры с requires_grad=True для автоматического вычисления градиентов
-# x = torch.tensor(2.0, requires_grad=True)
-# y = torch.tensor(3.0, requires_grad=True)
-# z = torch.tensor(4.0, requires_grad=True)
-#
-# print(f'x.requires_grad = {x.requires_grad}')
-# print(f'y.requires_grad = {y.requires_grad}')
-# print(f'z.requires_grad = {z.requires_grad}')
-#
-# # Вычисляем функцию f(x,y,z) = x² + y² + z² + 2xyz
-# f = x**2 + y**2 + z**2 + 2 * x * y * z
-# print(f'f (no grad) = {f}, f.requires_grad = {f.requires_grad}')
-#
-# # Вычисляем градиенты
-# f.backward()
-#
-# # Получаем градиенты
-# grad_x = x.grad
-# grad_y = y.grad
-# grad_z = z.grad
-#
-# print("Вычисленные градиенты:")
-# print(f"df/dx = {grad_x.item()}")
-# print(f"df/dy = {grad_y.item()}")
-# print(f"df/dz = {grad_z.item()}\n")
-#
-# # Аналитическое вычисление градиентов
-# def analytical_gradients(x_val, y_val, z_val):
-#     df_dx = 2*x_val + 2*y_val*z_val
-#     df_dy = 2*y_val + 2*x_val*z_val
-#     df_dz = 2*z_val + 2*x_val*y_val
-#     return df_dx, df_dy, df_dz
-#
-# # Вычисляем аналитические градиенты
-# a_grad_x, a_grad_y, a_grad_z = analytical_gradients(x.item(), y.item(), z.item())
-#
-# print("Аналитические градиенты:")
-# print(f"df/dx = {a_grad_x}")
-# print(f"df/dy = {a_grad_y}")
-# print(f"df/dz = {a_grad_z}\n")
-#
-# # Проверка совпадения результатов
-# assert torch.allclose(grad_x, torch.tensor(a_grad_x))
-# assert torch.allclose(grad_y, torch.tensor(a_grad_y))
-# assert torch.allclose(grad_z, torch.tensor(a_grad_z))
-#
-# print("Градиенты совпадают с аналитическими вычислениями!")
+# Создаем тензоры с requires_grad=True для автоматического вычисления градиентов
+x = torch.tensor(2.0, requires_grad=True)
+y = torch.tensor(3.0, requires_grad=True)
+z = torch.tensor(4.0, requires_grad=True)
+
+print(f'x.requires_grad = {x.requires_grad}')
+print(f'y.requires_grad = {y.requires_grad}')
+print(f'z.requires_grad = {z.requires_grad}')
+
+# Вычисляем функцию f(x,y,z) = x² + y² + z² + 2xyz
+f = x**2 + y**2 + z**2 + 2 * x * y * z
+print(f'f (no grad) = {f}, f.requires_grad = {f.requires_grad}')
+
+# Вычисляем градиенты
+f.backward()
+
+# Получаем градиенты
+grad_x = x.grad
+grad_y = y.grad
+grad_z = z.grad
+
+print("Вычисленные градиенты:")
+print(f"df/dx = {grad_x.item()}")
+print(f"df/dy = {grad_y.item()}")
+print(f"df/dz = {grad_z.item()}\n")
+
+# Аналитическое вычисление градиентов
+def analytical_gradients(x_val, y_val, z_val):
+    df_dx = 2*x_val + 2*y_val*z_val
+    df_dy = 2*y_val + 2*x_val*z_val
+    df_dz = 2*z_val + 2*x_val*y_val
+    return df_dx, df_dy, df_dz
+
+# Вычисляем аналитические градиенты
+a_grad_x, a_grad_y, a_grad_z = analytical_gradients(x.item(), y.item(), z.item())
+
+print("Аналитические градиенты:")
+print(f"df/dx = {a_grad_x}")
+print(f"df/dy = {a_grad_y}")
+print(f"df/dz = {a_grad_z}\n")
+
+# Проверка совпадения результатов
+assert torch.allclose(grad_x, torch.tensor(a_grad_x))
+assert torch.allclose(grad_y, torch.tensor(a_grad_y))
+assert torch.allclose(grad_z, torch.tensor(a_grad_z))
+
+print("Градиенты совпадают с аналитическими вычислениями!")
 
 # Задание 2.2
 
-# # Создаем данны:
-# x = torch.tensor([1.0, 2.0, 3.0, 4.0], requires_grad=False)
-# y_true = torch.tensor([2.5, 4.3, 6.1, 7.8], requires_grad=False)
-#
-# w = torch.tensor(0.5, requires_grad=True)
-# b = torch.tensor(0.0, requires_grad=True)
-#
-# # Реализация функции:
-# def linear_regression(x, w, b):
-#     return w * x + b
-#
-# # Реализация MSE loss:
-# def mse_loss(y_pred, y_true):
-#     return torch.mean((y_pred - y_true)**2)
-#
-# # Вычисляем:
-# y_pred = linear_regression(x, w, b)
-# loss = mse_loss(y_pred, y_true)
-#
-# # Вычисляем градиенты:
-# loss.backward()
-#
-# # Получаем градиенты
-# print(f"Градиент по w: {w.grad.item():.4f}")
-# print(f"Градиент по b: {b.grad.item():.4f}")
-#
-# # Аналитическая проверка градиентов:
-# n = len(x)
-# analytic_grad_w = (2/n) * torch.sum((y_pred - y_true) * x)
-# analytic_grad_b = (2/n) * torch.sum(y_pred - y_true)
-#
-# print("\nАналитические градиенты:")
-# print(f"∂MSE/∂w: {analytic_grad_w.item():.4f}")
-# print(f"∂MSE/∂b: {analytic_grad_b.item():.4f}")
-#
-# # Проверка совпадения:
-# assert torch.allclose(w.grad, analytic_grad_w, atol=1e-5)
-# assert torch.allclose(b.grad, analytic_grad_b, atol=1e-5)
-#
-# print("\nПроверка пройдена: градиенты совпадают!")
+# Создаем данны:
+x = torch.tensor([1.0, 2.0, 3.0, 4.0], requires_grad=False)
+y_true = torch.tensor([2.5, 4.3, 6.1, 7.8], requires_grad=False)
+
+w = torch.tensor(0.5, requires_grad=True)
+b = torch.tensor(0.0, requires_grad=True)
+
+# Реализация функции:
+def linear_regression(x, w, b):
+    return w * x + b
+
+# Реализация MSE loss:
+def mse_loss(y_pred, y_true):
+    return torch.mean((y_pred - y_true)**2)
+
+# Вычисляем:
+y_pred = linear_regression(x, w, b)
+loss = mse_loss(y_pred, y_true)
+
+# Вычисляем градиенты:
+loss.backward()
+
+# Получаем градиенты
+print(f"Градиент по w: {w.grad.item():.4f}")
+print(f"Градиент по b: {b.grad.item():.4f}")
+
+# Аналитическая проверка градиентов:
+n = len(x)
+analytic_grad_w = (2/n) * torch.sum((y_pred - y_true) * x)
+analytic_grad_b = (2/n) * torch.sum(y_pred - y_true)
+
+print("\nАналитические градиенты:")
+print(f"∂MSE/∂w: {analytic_grad_w.item():.4f}")
+print(f"∂MSE/∂b: {analytic_grad_b.item():.4f}")
+
+# Проверка совпадения:
+assert torch.allclose(w.grad, analytic_grad_w, atol=1e-5)
+assert torch.allclose(b.grad, analytic_grad_b, atol=1e-5)
+
+print("\nПроверка пройдена: градиенты совпадают!")
 
 # Задание 2.3
 
--- homework_tensors.py
+++ homework_tensors.py
@@ -1,104 +1,103 @@
 import torch
-import numpy as np
 
 # Задание 1.1
 
-# # Тензор 3x4 со случайными числами:
-# tensor1 = torch.rand(3, 4)
-# print("Тензор 3x4 со случайными числами:")
-# print(tensor1)
+# Тензор 3x4 со случайными числами:
+tensor1 = torch.rand(3, 4)
+print("Тензор 3x4 со случайными числами:")
+print(tensor1)
 
-# # Тензор размером 2x3x4, заполненный нулями
-# tensor2 = torch.zeros(2, 3, 4)
-# print("Тензор 2x3x4 с нулями:")
-# print(tensor2)
+# Тензор размером 2x3x4, заполненный нулями
+tensor2 = torch.zeros(2, 3, 4)
+print("Тензор 2x3x4 с нулями:")
+print(tensor2)
 
-# # Тензор размером 5x5, заполненный единицами
-# tensor3 = torch.ones(5, 5)
-# print("Тензор 5x5 с единицами:")
-# print(tensor3)
+# Тензор размером 5x5, заполненный единицами
+tensor3 = torch.ones(5, 5)
+print("Тензор 5x5 с единицами:")
+print(tensor3)
 
-# # Тензор размером 4x4 с числами от 0 до 15 (используя reshape)
-#tensor4 = torch.arange(16).reshape(4, 4)
-#print("Тензор 4x4 с числами от 0 до 15:")
-#print(tensor4)
+# Тензор размером 4x4 с числами от 0 до 15 (используя reshape)
+tensor4 = torch.arange(16).reshape(4, 4)
+print("Тензор 4x4 с числами от 0 до 15:")
+print(tensor4)
 
 
 # Задание 1.2
 
-# # Создаем тензоры A (3x4) и B (4x3):
-# A = torch.tensor([[1, 2, 3, 4],
-#                   [5, 6, 7, 8],
-#                   [9, 10, 11, 12]])
-#
-# B = torch.tensor([[1, 2, 3],
-#                   [4, 5, 6],
-#                   [7, 8, 9],
-#                   [10, 11, 12]])
+# Создаем тензоры A (3x4) и B (4x3):
+A = torch.tensor([[1, 2, 3, 4],
+                  [5, 6, 7, 8],
+                  [9, 10, 11, 12]])
+
+B = torch.tensor([[1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9],
+                  [10, 11, 12]])
 
 
-# # Если использовать случайные тензоры вместо заданных значений:
-# A = torch.rand(3, 4)
-# B = torch.rand(4, 3)
+# Если использовать случайные тензоры вместо заданных значений:
+A = torch.rand(3, 4)
+B = torch.rand(4, 3)
 
-# print("Тензор A:")
-# print(A)
-# print("\nТензор B:")
-# print(B)
+print("Тензор A:")
+print(A)
+print("\nТензор B:")
+print(B)
 
-# # Транспонирование тензора A:
-# A_transposed = A.T
-# print("\nТранспонированный тензор A:")
-# print(A_transposed)
+# Транспонирование тензора A:
+A_transposed = A.T
+print("\nТранспонированный тензор A:")
+print(A_transposed)
 
-# # Матричное умножение A и B
-# matrix_product = torch.matmul(A, B)  # или A @ B
-# print("\nМатричное произведение A и B:")
-# print(matrix_product)
+# Матричное умножение A и B
+matrix_product = torch.matmul(A, B)  # или A @ B
+print("\nМатричное произведение A и B:")
+print(matrix_product)
 
-# # Поэлементное умножение A и транспонированного B
-# # Сначала убедимся, что размерности совпадают (3x4 и 3x4)
-# B_transposed = B.T
-# elementwise_product = A * B_transposed
-# print("\nПоэлементное произведение A и B^T:")
-# print(elementwise_product)
-#
-# # Сумма всех элементов тензора A
-# sum_A = torch.sum(A)
-# print("\nСумма всех элементов тензора A:")
-# print(sum_A)
+# Поэлементное умножение A и транспонированного B
+# Сначала убедимся, что размерности совпадают (3x4 и 3x4)
+B_transposed = B.T
+elementwise_product = A * B_transposed
+print("\nПоэлементное произведение A и B^T:")
+print(elementwise_product)
+
+# Сумма всех элементов тензора A
+sum_A = torch.sum(A)
+print("\nСумма всех элементов тензора A:")
+print(sum_A)
 
 # Задание 1.3
 
-# # Создаем тензор 5x5x5 со значениями от 0 до 124 (5*5*5=125)
-# tensor = torch.arange(125).reshape(5, 5, 5)
-# print("Исходный тензор 5x5x5:")
-# print(tensor)
-# print(f"Размерность: {tensor.shape}\n")
-#
-# # Извлечение первой строки каждого слоя (матрицы)
-# first_rows = tensor[:, 0, :]  # Все слои, первая строка, все столбцы
-# print("Первая строка каждого слоя (размер 5x5):")
-# print(first_rows)
-# print(f"Размерность: {first_rows.shape}\n")
-#
-# # Извлечение последнего столбца каждого слоя
-# last_columns = tensor[:, :, -1]  # Все слои, все строки, последний столбец
-# print("Последний столбец каждого слоя (размер 5x5):")
-# print(last_columns)
-# print(f"Размерность: {last_columns.shape}\n")
-#
-# # Подматрица 2x2 из центра каждого слоя
-# center_submatrix = tensor[:, 1:3, 1:3]  # Все слои, строки 1-2, столбцы 1-2
-# print("Подматрица 2x2 из центра каждого слоя (размер 5x2x2):")
-# print(center_submatrix)
-# print(f"Размерность: {center_submatrix.shape}\n")
-#
-# # Все элементы с четными индексами по каждому измерению
-# even_indices = tensor[::2, ::2, ::2]  # Четные индексы по всем трем осям
-# print("Элементы с четными индексами по всем измерениям (размер 3x3x3):")
-# print(even_indices)
-# print(f"Размерность: {even_indices.shape}")
+# Создаем тензор 5x5x5 со значениями от 0 до 124 (5*5*5=125)
+tensor = torch.arange(125).reshape(5, 5, 5)
+print("Исходный тензор 5x5x5:")
+print(tensor)
+print(f"Размерность: {tensor.shape}\n")
+
+# Извлечение первой строки каждого слоя (матрицы)
+first_rows = tensor[:, 0, :]  # Все слои, первая строка, все столбцы
+print("Первая строка каждого слоя (размер 5x5):")
+print(first_rows)
+print(f"Размерность: {first_rows.shape}\n")
+
+# Извлечение последнего столбца каждого слоя
+last_columns = tensor[:, :, -1]  # Все слои, все строки, последний столбец
+print("Последний столбец каждого слоя (размер 5x5):")
+print(last_columns)
+print(f"Размерность: {last_columns.shape}\n")
+
+# Подматрица 2x2 из центра каждого слоя
+center_submatrix = tensor[:, 1:3, 1:3]  # Все слои, строки 1-2, столбцы 1-2
+print("Подматрица 2x2 из центра каждого слоя (размер 5x2x2):")
+print(center_submatrix)
+print(f"Размерность: {center_submatrix.shape}\n")
+
+# Все элементы с четными индексами по каждому измерению
+even_indices = tensor[::2, ::2, ::2]  # Четные индексы по всем трем осям
+print("Элементы с четными индексами по всем измерениям (размер 3x3x3):")
+print(even_indices)
+print(f"Размерность: {even_indices.shape}")
 
 # Задание 1.4
 
